49d686d6c92a4ed5b80864b4662aa07613a99462
Gonzalo Andreani <gonzaandreani@gmail.com>
2020-05-31 19:28:06 -0400

    Add rocket league replays to repo

--- RocketLeagueReplayParser.Tests/ReplayFileSource.cs
+++ RocketLeagueReplayParser.Tests/ReplayFileSource.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace RocketLeagueReplayParser.Tests
 {
@@ -13,8 +14,10 @@ namespace RocketLeagueReplayParser.Tests
         {
             get
             {
-                var dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"My Games\Rocket League\TAGame\Demos\");
-                return Directory.EnumerateFiles(dir, "*.replay").OrderByDescending(x => File.GetCreationTime(x))
+                
+                var dir = Path.Combine(Environment.CurrentDirectory, "TestReplays");
+                return Directory.EnumerateFiles(dir, "*.replay")
+                    .OrderByDescending(x => File.GetCreationTime(x))
                     // Ignore this replay. It crashes RL too
                     .Where(f => !f.Contains("B82DDB624C393A4A425E68AB40DC2450"));
             }
